ef24a60aa35cb03ebcd2122fc317cc3ec3b459f0
zhiyuan1146 <C113181116@nkust.edu.tw>
2026-03-05 11:06:09 +0800

    1

---
+++ ex02.java
@@ -0,0 +1,18 @@
+public class ex02 {
+    public static void main(String[] args) {
+
+        int grade = 80;
+        char type = 'm';
+
+        if (grade >= 60 && type == 'm') {
+            System.out.println("課程 " + type + " 成績及格\n" + grade);
+        } 
+        else {
+            System.out.println("課程不正確或成績不及格");
+        }
+    }
+
+    public static void main2(String[] args) {
+
+    }
+}
---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+
+        int grade = 80;
+
+        if (grade > 60) {
+            System.out.println("成績及格\n" + grade);
+        }
+    }
+}
